2352d4a5cb8d8b1c5ead148db648f70b88775ddf
unknown <brunosznorth@gmail.com>
2022-08-28 16:14:44 -0300

    Laco de repeticao - Contagem regressiva: bomba

---
+++ Logica e OO/Bomba.py
@@ -0,0 +1,6 @@
+import time
+print("\n Iniciando contagem regressiva! \n")
+for x in range(10):
+    print(x + 1, "! \n")
+    time.sleep(1)
+print("\n\n\n#################  BUM!  #################\n\n\n")
