2cc6c19997afbf23e0a6c8c89393e1d7b14e7e30
bruno-npc <brunosznorth@gmail.com>
2022-09-21 23:13:40 -0300

    Up Projeto - Manipulacao de String

---
+++ Logica e OO/Manipulação de String/Manipulacao.py
@@ -0,0 +1,16 @@
+#Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+#Exibir string
+s = "Olá, mundo!"
+print(s)
+
+# Tamanho da string
+len(s)
+
+# Concatenação de string
+print("Concate" + "nação")
+
+# Substitui uma substring por outra
+s1 = s.replace("mundo", "meu caro")
+
+print(s1)
