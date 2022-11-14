8fd749e1e2447dd7a60977498ff3820e5ca5161b
Bruno Souza <61155961+bruno-npc@users.noreply.github.com>
2022-11-14 15:37:01 -0300

    Funcoes tradicionais e arrow function

---
+++ Javascript/Funcoes/Funcoes.js
@@ -0,0 +1,9 @@
+//1. uma função tradicional com a palavra reservada “Function” e sem nenhum parâmetro;
+//2. uma função tradicional com parâmetros e um retorno de valor;
+//3. uma arrow function com parâmetros e que retorne um valor.
+
+function vazio (){}
+
+function parametros( x, y){return (x+y)}
+
+const soma = (x, y) => (x+y)
