04b7317b579a09f830cc0aba1a393e38a7c49f47
Bruno Souza <61155961+bruno-npc@users.noreply.github.com>
2022-11-14 18:14:48 -0300

    Requisicao HTTP get nodejs

---
+++ HTTP/Requisicao.js
@@ -0,0 +1,19 @@
+//Crie um código JavaScript no back-end utilizando o Node.JS. No código, 
+//você deverá elaborar uma ou mais rotas, que podem ser de qualquer tipo (get, put etc). 
+//Depois, faça um teste em aplicações de rota, como o Postman ou o Insomnia, para confirmar se o 
+//retorno está coerente com o que você programou.
+
+//Observação: os prints, ou o próprio código, devem ser divididos por arquivos. 
+//Por exemplo, o código de rotas está em um arquivo diferente do código de conexão.
+
+import express from 'express'
+
+const express = require('express');
+
+const app = express();
+
+app.listen(8080);
+
+app.get('/hello', (req, resp)) => {
+    return resp.send('Hello World!');
+});
